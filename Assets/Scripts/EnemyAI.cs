d90d645c9bf018d5c517dee3ae71538f0c16d5c8
kazusa7 <7218455+kazusa7@users.noreply.github.com>
2020-11-04 23:54:09 +0900

    Add: EnemyAIクラスの追加(WIP)

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI
+{
+	public PieceMoveInfo GetMoveInfo(Square[,] board)
+	{
+		PieceMoveInfo moveInfo = new PieceMoveInfo();
+
+		Square moveFrom = CalcMoveFromSquare(board);
+		Square moveTo = CalcMoveToSquare(board, moveFrom);
+
+		moveInfo.SetMoveFrom(moveFrom);
+		moveInfo.SetMoveTo(moveTo.Address);
+
+		return moveInfo;
+	}
+
+	public Square CalcMoveFromSquare(Square[,] board)
+	{
+		return board[1, 3];
+	}
+
+	public Square CalcMoveToSquare(Square[,] board, Square from)
+	{
+		return board[1, 4];
+	}
+}
